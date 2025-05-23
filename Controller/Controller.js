c5d6e13bc971502d03562604b944f7d4e6fffb36
Anandhbasha <anadhakm@gmail.com>
2025-05-23 09:23:52 +0530

    read

--- Controller/Controller.js
+++ Controller/Controller.js
@@ -6,16 +6,31 @@ export const readData = async(req,res)=>{
 }
 export const insertData = async(req,res)=>{
     const {userName,userEmail} = req.body
-    const insertNewData = await user.insertOne({userName,userEmail})
+    const checkData = await user.findOne({userEmail})
+    if(checkData){
+        return res.status(406).json(`User Already exist`)
+    }
+    const inserNewData = await user({userName,userEmail}).save()
     return res.status(202).send(`The user Name is ${userName} Email is ${userEmail}`)
 }
-export const updateData = (req, res) => {
-    const { id } = req.params;
-    const { age } = req.body;
-    res.status(201).json(`User ${id}'s new age (${age}) updated successfully.`);
+ export const update = async (req,res) => {
+    try {
+        const {userEmail} =  req.params
+        const {userName} = req.body
+        const updatedata = await user.findOneAndUpdate({userEmail:userEmail},{$set:{userName}})
+        if(!updatedata){
+            res.status(404).json("Doesn't Exist")
+        }
+        
+        res.status(202).json({message:"updated Successfully",
+            data: updatedata})
+    } catch (error) {
+        res.status(500).json(error)
+    }
 }
-export const deleteData = (req,res)=>{
-    const { id } = req.params;
-    res.status(201).json(`User ${id}'s Deleted Sucessfully.`);
+export const deleteData = async(req,res)=>{
+    const { userEmail } = req.params;
+    const checkExist = await user.findOneAndDelete({userEmail:userEmail})
+    res.status(201).json(`User ${userEmail}'s Deleted Sucessfully.`);
 
 }
--- Router/Route.js
+++ Router/Route.js
@@ -1,10 +1,10 @@
 import express from 'express'
-import { deleteData, insertData, readData, updateData } from '../Controller/Controller.js';
+import { deleteData, insertData, readData, update } from '../Controller/Controller.js';
 const route = express.Router()
 
 route.get('/read',readData)
 
 route.post('/insert',insertData)
-route.put('/update/:id', updateData);
-route.delete('/deleteuser/:id',deleteData)
+route.put('/update/:userEmail', update);
+route.delete('/deleteuser/:userEmail',deleteData)
 export default route
